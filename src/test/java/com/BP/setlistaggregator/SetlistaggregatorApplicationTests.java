f89ae9c76ce4785fb3cb3d28868b9afa0c977781
Braden <braden.prestholdt@uwrf.edu>
2025-05-17 01:28:04 -0500

    Final push before submodule conversion

--- src/test/java/com/BP/setlistaggregator/SetlistaggregatorApplicationTests.java
+++ src/test/java/com/BP/setlistaggregator/SetlistaggregatorApplicationTests.java
@@ -2,8 +2,13 @@ package com.BP.setlistaggregator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+//forces Spring to use application-test.properties from src/test/resources
+//so we know if the app works independent of deployment config
+@ActiveProfiles("test")
 class SetlistaggregatorApplicationTests {
 
 	@Test
