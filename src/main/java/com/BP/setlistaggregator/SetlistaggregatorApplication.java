33331a7bc02168a082fa1e63cd3644ac2c8b567f
Braden <braden.prestholdt@uwrf.edu>
2025-05-17 02:51:20 -0500

    Fix environment variables

--- src/main/java/com/BP/setlistaggregator/SetlistaggregatorApplication.java
+++ src/main/java/com/BP/setlistaggregator/SetlistaggregatorApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SetlistaggregatorApplication {
 
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SetlistaggregatorApplication.class, args);
 	}
 
@@ -17,5 +20,13 @@ public class SetlistaggregatorApplication {
 	@Bean
 	public WebClient.Builder webClientBuilder() {
 		return WebClient.builder();
+
+	}
+
+	@PostConstruct
+	public void logDatasourceEnvVars() {
+		System.out.println("🔍 SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("🔍 SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+		System.out.println("🔍 SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
 	}
 }
