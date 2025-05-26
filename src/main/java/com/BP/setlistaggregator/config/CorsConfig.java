4ddb66c89bb0cecc4de9cd3d7cf2c4e0af4f6dd8
Braden <braden.prestholdt@uwrf.edu>
2025-05-26 12:22:58 -0500

    cors fixes for deployment

--- src/main/java/com/BP/setlistaggregator/config/CorsConfig.java
+++ src/main/java/com/BP/setlistaggregator/config/CorsConfig.java
@@ -7,12 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://setlist-aggregator-frontend.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
             }
         };
     }
